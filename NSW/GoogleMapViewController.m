6a3b2b198126d91abfd9580868aa54aa7353ada3
Lab User <labuser@wcc138-08r.its.carleton.edu>
2014-06-03 21:32:56 -0500

    started adding map title bar

--- NSW/GoogleMapViewController.m
+++ NSW/GoogleMapViewController.m
@@ -8,9 +8,11 @@
 
 #import "GoogleMapViewController.h"
 #import <GoogleMaps/GoogleMaps.h>
+#import "SWRevealViewController.h"
+#import "NSWStyle.h"
 
 @interface GoogleMapViewController ()
-
+@property (nonatomic) IBOutlet UIBarButtonItem *revealButtonItem;
 @end
 
 @implementation GoogleMapViewController{
@@ -30,14 +32,41 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    UIStoryboard *aStoryboard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
+    
+    mapView_ = [aStoryboard instantiateViewControllerWithIdentifier:@"gMap"];
+    
+    
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:44.461
                                                             longitude:-93.1546
                                                                  zoom:17];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView_.myLocationEnabled = YES;
     self.view = mapView_;
-
+    
+    
+    self.navigationItem.title = @"Map of Campus";
+    [self.revealButtonItem setTarget: self.revealViewController];
+    [self.revealButtonItem setAction: @selector( revealToggle: )];
+    [self.navigationController.navigationBar addGestureRecognizer: self.revealViewController.panGestureRecognizer];
+    [self setNavigationColors];
+    self.view.backgroundColor = [NSWStyle lightBlueColor];
 	// Do any additional setup after loading the view.
+}
+
+
+//This is directly lifted from BaseNSWTableViewController, can we put it in an interface or such?
+- (void)setNavigationColors{
+    UINavigationBar *navBar = self.navigationController.navigationBar;
+    navBar.translucent = NO;
+    navBar.barTintColor = [NSWStyle lightBlueColor];
+    [navBar setTitleTextAttributes:@{
+                                     NSForegroundColorAttributeName : [NSWStyle whiteColor],
+                                     NSFontAttributeName : [NSWStyle boldFont]}];
+    UIBarButtonItem *barBtnItem = [[UIBarButtonItem alloc]initWithTitle:@"" style:UIBarButtonItemStyleBordered target:self action:@selector(popoverArrowDirection)];
+    self.navigationItem.backBarButtonItem = barBtnItem;
+    //"navBar text color" = [NSWStyle whiteColor];
+    self.revealButtonItem.tintColor = [NSWStyle whiteColor];
 }
 
 - (void)didReceiveMemoryWarning
